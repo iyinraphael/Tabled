54b02a8b10b3382175890c973bcfbf8268232ebc
iyinraphael <iyinraphael@gmail.com>
2018-11-08 14:28:49 -0800

    storyboard was created and hooked up to view controller

---
+++ Tabled/Tabled/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Tabled
+//
+//  Created by Iyin Raphael on 11/8/18.
+//  Copyright © 2018 Iyin Raphael. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ Tabled/Tabled/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Tabled
+//
+//  Created by Iyin Raphael on 11/8/18.
+//  Copyright © 2018 Iyin Raphael. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let reuseidentifier = "cell"
+    
+    @IBAction func add(_ sender: Any) {
+    }
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var textField: UITextField!
+    
+}
+
